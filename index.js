3a96c125aff2ebfa9c6fb696e3dc96993023a4c5
vikash rahii <vikashrahii999@gmail.com>
2020-11-14 03:55:45 +0530

    making routes

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const connectDB = require("./config/key");
 const app = express();
+const routes = require('./routing/routes')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use("/routing/routes", require("./routing/routes"));
+
 
 // db config
 const db = require("./config/key").mongoURI;
